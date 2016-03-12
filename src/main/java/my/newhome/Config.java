4b74267ce10ac221471760017c76a2753dc68095
MeinUsername <floriankra2007@gmail.com>
2016-03-12 17:18:37 +0100

    Initial Commit
    - initial commit
    - gitignore

---
+++ src/main/java/my/newhome/Config.java
@@ -0,0 +1,14 @@
+package my.newhome;
+
+/**
+ * Created by Florian on 12.03.2016.
+ */
+public class Config {
+
+    public Config(String FileName)
+    {
+
+    }
+
+
+}
---
+++ src/main/java/my/newhome/Main.java
@@ -0,0 +1,9 @@
+package my.newhome;
+
+public class Main {
+
+    public static void main(String[] args) {
+	  System.out.println("Find a new home with Google Maps Distance Matrix");
+        
+    }
+}
